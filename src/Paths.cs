93e358190c8fbef6eb7055a8d2a37801225ca413
Chris Banwarth <github@aprettycoolprogram.com>
2025-01-23 14:46:19 -0500

    Update Paths.cs

--- src/Paths.cs
+++ src/Paths.cs
@@ -15,8 +15,8 @@ namespace TingenCommander
         public string CmdrConfigFile { get; set; }
         public string TngnDataRoot { get; set; }
         public string TngnAdminModePath { get; set; }
-        public string LiveDetailFile { get; set; }
-        public string UatDetailFile { get; set; }
+        public string LiveCommanderFile { get; set; }
+        public string UatCommanderFile { get; set; }
 
         /// <summary>Create the initial Tingen Commander paths.</summary>
         /// <param name="paths">The dictionary that contains  the Tingen Commander paths.</param>
@@ -34,13 +34,13 @@ namespace TingenCommander
         ///   </para>
         ///   <para>
         ///   By design, any server that hosts the Tingen web service has a directory named "C:\TingenData", where<br/>
-        ///   Tingen-related data is stored. If you run Tingen Commander on a machine that has a "C:\TingenData", Tingen<br/>
-        ///   Commander will run in "Administrator  Mode", which enables all functionality.<br/>
+        ///   Tingen-related data is stored. If you run Tingen Commander on a machine that has a "C:\TingenData",<br/>
+        ///   Tingen Commander will run in "Administrator Mode", which enables all functionality.<br/>
         ///   <br/>
         ///   If the "C:\TingenData" directory does not exist, Tingen Commander will run in "Standard User Mode".
         ///   </para>
         /// </remarks>
-        /// <returns>A dictionary containing the initial Tingen Commander paths. </returns>
+        /// <returns>A dictionary containing the initial Tingen Commander paths.</returns>
         internal static Paths Initialize(Paths paths)
         {
             /* The cmdrRoot is hard-coded as ".\AppData", and should not be modified.
@@ -54,13 +54,36 @@ namespace TingenCommander
             return paths;
         }
 
+        /// <summary> Update the Tingen Commander paths dictionary.</summary>
+        /// <param name="paths">The dictionary that contains  the Tingen Commander paths.</param>
+        /// <param name="tngnData">The path to Tingen Data location.</param>
+        /// <remarks>
+        ///   <para>
+        ///     The Paths.Initialize() method creates the initial Tingen Commander paths, and this method builds upon those,
+        ///     concentrating on the following:
+        ///     <list type="bullet">
+        ///       <item>TngnDataRoot:</item>
+        ///       <description>The root directory all Tingen web service data.</description>
+        ///       <item>LiveCommanderFile:</item>
+        ///       <description>The full path to the "LIVE.commander" file.</description>
+        ///       <item>UatCommanderFile:</item>
+        ///       <description>The full path to the "UAT.commander" file..</description>
+        ///     </list>
+        ///   </para>
+        ///   <para>
+        ///     We need to know the true value of <c>TngnDataRoot</c>, in order to set most of the remaining paths.<br/>
+        ///     <c>TngnDataRoot</c> can either be <c>"C:\TingenData"</c> or the path defined in the configuration<br/>
+        ///     file, depending which exists (or if <c>TngnDataRoot</c> exists first.
+        ///   </para>
+        /// </remarks>
+        /// <returns>A dictionary containing updated Tingen Commander paths.</returns>
         internal static Paths Update(Paths paths, string tngnData)
         {
-            var tngnDataPath = SetTngnDataPath(paths.TngnAdminModePath, tngnData);
+            var trueTngnDataRoot = SetTngnDataPath(paths.TngnAdminModePath, tngnData);
 
-            paths.TngnDataRoot   = tngnDataPath;
-            paths.LiveDetailFile = $@"{tngnDataPath}\Admin\LIVE.commander";
-            paths.UatDetailFile  = $@"{tngnDataPath}\Admin\UAT.commander";
+            paths.TngnDataRoot      = trueTngnDataRoot;
+            paths.LiveCommanderFile = $@"{trueTngnDataRoot}\Admin\LIVE.commander";
+            paths.UatCommanderFile  = $@"{trueTngnDataRoot}\Admin\UAT.commander";
 
             return paths;
         }
